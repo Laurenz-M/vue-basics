3bf99d3723ccfb6315bc6159d033c3b54ad46130
Laurenz-M <laurenz.morbe@gmail.com>
2022-05-03 17:35:17 +0200

    Added simple button

---
+++ src/MyVueApp.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      buttonPressedCount: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.buttonPressedCount++;
+    },
+  },
+};
+</script>
+
+<template>
+  <h>You clicked the Button below {{ buttonPressedCount }} times! </h>
+  <button v-on:Click="increment">Click me!</button>
+</template>
+
+<style></style>
